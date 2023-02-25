fad3aa9ec6dbd3cd2e6d563dc5781cb7195a88ce
Mehmet Karanlık <89414084+MehmetKaranlik@users.noreply.github.com>
2023-02-25 16:40:38 +0300

    Initial Commit

---
+++ Sources/Core/Extensions/View/UIHostingController+Extensions.swift
@@ -0,0 +1,21 @@
+//
+//  UIHostingController+Extensions.swift
+//  UIKit+SwiftUI+Architecture
+//
+//  Created by mehmet karanlık on 20.02.2023.
+//
+
+import Foundation
+import SwiftUI
+
+@available(iOS 13.0, *)
+extension UIHostingController {
+
+   func addToVC(target : UIViewController) -> Void {
+      target.addChild(self)
+      self.view.frame = target.view.frame
+      target.view.addSubview(self.view)
+      self.didMove(toParent: target)
+   }
+
+}
---
+++ Sources/Core/Extensions/View/View+Extensions.swift
@@ -0,0 +1,17 @@
+//
+//  View+Extensions.swift
+//  UIKit+SwiftUI+Architecture
+//
+//  Created by mehmet karanlık on 20.02.2023.
+//
+
+import Foundation
+import SwiftUI
+
+
+@available(iOS 13.0, *)
+extension View {
+   func toHostingController() -> UIHostingController<Self> {
+      return UIHostingController<Self>(rootView: self)
+   }
+}

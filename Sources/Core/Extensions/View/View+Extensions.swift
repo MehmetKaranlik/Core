3e9b037f5a5d410c6cd69e4941633d285e1cb4cf
Mehmet Karanlık <89414084+MehmetKaranlik@users.noreply.github.com>
2023-03-06 01:23:17 +0300

    Add keyboard listener

--- Sources/Core/Extensions/View/View+Extensions.swift
+++ Sources/Core/Extensions/View/View+Extensions.swift
@@ -7,11 +7,26 @@
 
 import Foundation
 import SwiftUI
-
+import Combine
 
 @available(iOS 13.0, *)
 public extension View {
    func toHostingController() -> UIHostingController<Self> {
       return UIHostingController<Self>(rootView: self)
+   }
+
+      var keyboardPublisher: AnyPublisher<Bool, Never> {
+         Publishers
+            .Merge(
+               NotificationCenter
+                  .default
+                  .publisher(for: UIResponder.keyboardWillShowNotification)
+                  .map { _ in true },
+               NotificationCenter
+                  .default
+                  .publisher(for: UIResponder.keyboardWillHideNotification)
+                  .map { _ in false })
+            .debounce(for: .seconds(0.1), scheduler: RunLoop.main)
+            .eraseToAnyPublisher()
    }
 }
